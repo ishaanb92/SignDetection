a3bdea532fec624b2ba48b0141654c824d5d6044
kilgore92 <ishaan.bits92@gmail.com>
2017-06-07 11:05:01 +0200

    Fixing the frame extractor script

--- create_neg_samples.py
+++ create_neg_samples.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import os
 
-vidCapture = cv2.VideoCapture('neg_frames.avi')
+vidCapture = cv2.VideoCapture('neg_frames.mp4')
 # Read first frame
 success, image = vidCapture.read()
 print success
@@ -14,6 +14,8 @@ if not os.path.isdir(os.path.join(os.getcwd(),'neg_samples')):
 
 while (vidCapture.isOpened()):
     success,image = vidCapture.read()
+    if not success:
+        continue
     if count >= 5000:
         break; # 5000 negative samples needed
     cv2.imwrite(os.path.join("neg_samples","frame{:d}.jpg".format(count)),image)
