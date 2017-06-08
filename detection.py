d6f39f000b95d5e7468d8c8eb3c452305c55c09a
kilgore92 <ishaan.bits92@gmail.com>
2017-06-08 16:32:53 +0200

    Tested video with both right and left signs. Both signs get detected fine

--- detection.py
+++ detection.py
@@ -7,8 +7,11 @@ import os
 left_classifier_path = os.path.join(os.getcwd(),'classifier_left_sign','cascade.xml')
 left_sign_classifier = cv2.CascadeClassifier(left_classifier_path)
 
+right_classifier_path = os.path.join(os.getcwd(),'classifier_right_sign','cascade.xml')
+right_sign_classifier = cv2.CascadeClassifier(right_classifier_path)
+
 # Read test img
-cap = cv2.VideoCapture('sign_test.mp4')
+cap = cv2.VideoCapture('dual_sign_test.mp4')
 
 while (cap.isOpened()):
     ret,frame = cap.read()
@@ -16,8 +19,13 @@ while (cap.isOpened()):
     frameResizeGray = cv2.cvtColor(frameResize,cv2.COLOR_BGR2GRAY)
 # Detect the "left" sign
     left_sign = left_sign_classifier.detectMultiScale(frameResizeGray,1.05,3)
+
+    right_sign = right_sign_classifier.detectMultiScale(frameResizeGray,1.05,3)
     for (x,y,w,h) in left_sign:
         cv2.rectangle(frameResize,(x,y),(x+w,y+h),(0,255,0),2)
+
+    for (x,y,w,h) in right_sign:
+        cv2.rectangle(frameResize,(x,y),(x+w,y+h),(0,0,255),2)
 
     cv2.imshow('Detected',frameResize)
     if cv2.waitKey(1) & 0xFF == ord('q'):
