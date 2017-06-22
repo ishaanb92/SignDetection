1705a5c858657855e4790a3e77e1f5e91e6df2a2
kilgore92 <ishaan.bits92@gmail.com>
2017-06-22 12:40:27 +0200

    Renaming stop sign cascade classifier, fixing detection.py to resize captured video frames

--- detection.py
+++ detection.py
@@ -10,8 +10,11 @@ left_sign_classifier = cv2.CascadeClassifier(left_classifier_path)
 right_classifier_path = os.path.join(os.getcwd(),'classifier_right_sign','cascade.xml')
 right_sign_classifier = cv2.CascadeClassifier(right_classifier_path)
 
+stop_classifier_path = os.path.join(os.getcwd(),'classifier_stop_sign','cascade.xml')
+stop_sign_classifier = cv2.CascadeClassifier(stop_classifier_path)
+
 # Read test img
-cap = cv2.VideoCapture('dual_sign_test.mp4')
+cap = cv2.VideoCapture('video_new.h264')
 
 while (cap.isOpened()):
     ret,frame = cap.read()
@@ -19,15 +22,22 @@ while (cap.isOpened()):
     frameResizeGray = cv2.cvtColor(frameResize,cv2.COLOR_BGR2GRAY)
 # Detect the "left" sign
     left_sign = left_sign_classifier.detectMultiScale(frameResizeGray,1.05,3)
-
+# Detect the "right" sign
     right_sign = right_sign_classifier.detectMultiScale(frameResizeGray,1.05,3)
+# Detect the "stop" sign
+    stop_sign = stop_sign_classifier.detectMultiScale(frameResizeGray,1.05,3)
+
     for (x,y,w,h) in left_sign:
         cv2.rectangle(frameResize,(x,y),(x+w,y+h),(0,255,0),2)
 
     for (x,y,w,h) in right_sign:
         cv2.rectangle(frameResize,(x,y),(x+w,y+h),(0,0,255),2)
 
+    for (x,y,w,h) in stop_sign:
+        cv2.rectangle(frameResize,(x,y),(x+w,y+h),(255,0,0),2)
+
     cv2.imshow('Detected',frameResize)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
