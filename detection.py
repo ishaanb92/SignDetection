3c540fd6e722530924877a5895d67bfe9cb4188e
kilgore92 <ishaan.bits92@gmail.com>
2017-07-26 20:45:52 +0200

    Making changes for Pim

--- detection.py
+++ detection.py
@@ -5,7 +5,9 @@ import os
 
 # load haar cascade and street image
 left_classifier_path = os.path.join(os.getcwd(),'classifier_left_sign','cascade.xml')
-left_sign_classifier = cv2.CascadeClassifier(left_classifier_path)
+#left_sign_classifier = cv2.CascadeClassifier(left_classifier_path)
+
+left_sign_classifier = cv2.CascadeClassifier('cascade_pim.xml')
 
 right_classifier_path = os.path.join(os.getcwd(),'classifier_right_sign','cascade.xml')
 right_sign_classifier = cv2.CascadeClassifier(right_classifier_path)
@@ -14,10 +16,11 @@ stop_classifier_path = os.path.join(os.getcwd(),'classifier_stop_sign','cascade.
 stop_sign_classifier = cv2.CascadeClassifier(stop_classifier_path)
 
 # Read video from USB webcam
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture('dual_sign_test.mp4')
 
 while (cap.isOpened()):
     ret,frameResize = cap.read()
+    frameResize = cv2.resize(frameResize,(640,480), interpolation = cv2.INTER_CUBIC)
     frameResizeGray = cv2.cvtColor(frameResize,cv2.COLOR_BGR2GRAY)
 # Detect the "left" sign
     left_sign = left_sign_classifier.detectMultiScale(frameResizeGray,1.05,3)
@@ -30,10 +33,10 @@ while (cap.isOpened()):
         cv2.rectangle(frameResize,(x,y),(x+w,y+h),(0,255,0),2)
 
     for (x,y,w,h) in right_sign:
-        cv2.rectangle(frameResize,(x,y),(x+w,y+h),(0,0,255),2)
+        cv2.rectangle(frameResize,(x,y),(x+w,y+h),(255,0,0),2)
 
     for (x,y,w,h) in stop_sign:
-        cv2.rectangle(frameResize,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(frameResize,(x,y),(x+w,y+h),(0,0,255),2)
 
     cv2.imshow('Detected',frameResize)
 
